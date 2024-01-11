6e807778f6c1cf6fc688d3460f46791fc18ea44b
David Hopkins II <dhscience2@gmail.com>
2024-01-11 03:46:40 -0500

    stopped in the middle of command handling

--- bot.js
+++ bot.js
@@ -1,12 +1,39 @@
 console.log("I am alive!")
+const fs = require("node:fs")
+const path = require("node:path")
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js") //importing Discord.js
+const { token } = require("./config.json")
 
-require("dotenv").config() //importing dotenv
-const Discord = require("discord.js") //importing Discord.js
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+}) //proceeds to create a new client for the bot
 
-const client = new Discord.Client() //proceeds to create a new client for the bot
+client.commands = new Collection()
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+const foldersPath = path.join(__dirname, "commands")
+const commandFolders = fs.readdirSync(foldersPath)
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder)
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"))
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command)
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      )
+    }
+  }
+}
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`)
 })
 
-client.login(process.env.CLIENT_TOKEN)
+client.login(token)
