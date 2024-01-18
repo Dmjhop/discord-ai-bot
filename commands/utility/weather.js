c36fb5d7da567d0961310791970afd952ba1f29b
David Hopkins II <dhscience2@gmail.com>
2024-01-17 23:53:32 -0500

    got the weather command working

--- commands/utility/weather.js
+++ commands/utility/weather.js
@@ -14,12 +14,16 @@ module.exports = {
   async execute(interaction) {
     const input = await interaction.options.get("input")
 
+    console.log(`\n \n This was the input ${input.value} \n \n`)
+
     const weatherData = await getWeather(input.value)
+
+    console.log(weatherData)
 
     await interaction.reply(
       `The weather in ${weatherData.name} is the following \n
-      The current temperature is ${weatherData.main.temp} \n
-      The high today will be ${weatherData.main.temp_max} and the low will be ${weatherData.main.temp_min} \n
+      The current temperature is ${weatherData.main.temp}°F \n
+      The high today will be ${weatherData.main.temp_max}°F and the low will be ${weatherData.main.temp_min}°F \n
       `
     )
   },
